2ecd23f7eceb1393b13be2e0e6fc4f7d29bd5f22
ririndeismillia <ririndeismillia@gmail.com>
2026-03-31 10:52:51 +0700

    void mergeSort1

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -30,3 +30,16 @@ void input() {
         cin >> arr[i];
     }
 }
+
+void mergeSort(int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+
+    int mid = (low + high) / 2;
+
+
+    
+}
